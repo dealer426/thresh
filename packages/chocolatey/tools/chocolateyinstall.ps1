cc96d6109d51d9ea1143fc4cdfb7e924477f0cfc
dealer426 <burnssamuel01@gmail.com>
2026-02-27 18:06:00 -0500

    chore: Update package manifests to v1.5.0
    - Updated Chocolatey package to v1.5.0 with SHA256 hash
    - Updated Scoop manifest to v1.5.0 with SHA256 hash
    - Updated WinGet manifests to v1.5.0 with SHA256 hash
    - Changed release date to 2026-02-27
    - Updated release notes URLs to v1.5.0
    - Updated download URLs to v1.5.0 release binaries
    
    Hash: deed76e07698f5e152b0e250eb5626af5b00381f13136e8877d95b4b5e5f35f1

--- packages/chocolatey/tools/chocolateyinstall.ps1
+++ packages/chocolatey/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'thresh'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/dealer426/thresh/releases/download/v1.0.0/thresh-windows-x64.zip'
-$checksum64 = '<TO_BE_UPDATED_AFTER_RELEASE>'
+$url64 = 'https://github.com/dealer426/thresh/releases/download/v1.5.0/thresh-windows-x64.zip'
+$checksum64 = 'deed76e07698f5e152b0e250eb5626af5b00381f13136e8877d95b4b5e5f35f1'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
